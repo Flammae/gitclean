b4d421ec90def0755652f0b8629e39c8daa04381
beqa <b.meqvabishvili159@gmail.com>
2018-03-11 08:49:14 -0800

    added error and success messages

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 
 const file = process.argv[2];
+if (!file) {
+  console.log("please provide file");
+} else {
+  fs.readFile(file, (err, data) => {
+    const lines = data.toString().split("\n");
+    const newLines = [];
 
-fs.readFile(file, (err, data) => {
-  if (err) console.log(err);
-  const lines = data.toString().split("\n");
-  const newLines = [];
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      const trimedLine = line.trim();
 
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    const trimedLine = line.trim();
-
-    if (trimedLine.charAt(0) === "+") {
-      const plusIndex = line.indexOf("+");
-      const newLine = line.slice(plusIndex + 1);
-      newLines.push(newLine);
-    } else if (trimedLine.charAt(0) !== "-") {
-      newLines.push(line);
+      if (trimedLine.charAt(0) === "+") {
+        const plusIndex = line.indexOf("+");
+        const newLine = line.slice(plusIndex + 1);
+        newLines.push(newLine);
+      } else if (trimedLine.charAt(0) !== "-") {
+        newLines.push(line);
+      }
+      // and if charAt(0) === "-", don't add the line in new document;
     }
-    // and if charAt(0) === "-", don't add the line in new document;
-  }
 
-  const fileToWrite = newLines.join("\n");
-  fs.writeFile(file, fileToWrite, err => {
-    if (err) console.log(err);
+    const fileToWrite = newLines.join("\n");
+    fs.writeFile(file, fileToWrite, err => {
+      if (err) console.log(err);
+      console.log("file rewritten successfully");
+    });
   });
-});
+}
